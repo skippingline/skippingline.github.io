d124f1c7698e7d0c1373fd9f7059a819dcbe9b09
builder <builder@homebase>
2020-06-19 20:26:00 +0000

    okay header

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,8 @@ import Logo from './Asset 11.svg';
 const Header = () => {
     return (
         <header className="row">
-            <div className="logoContainer col-xs-6"><img src={Logo} alt="Company Logo" className="logo" /></div>
-            <p className="col-xs-6">his is the header</p>
+            <div className="logoContainer col-xs-6 valign"><img src={Logo} alt="Company Logo" className="logo" /></div>
+            <p className="col-xs-offset-4 col-xs-2  calign">💣</p>
         </header >);
 }
 
