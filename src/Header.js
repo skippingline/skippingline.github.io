8bce5ca53ae37ad4c6b3d181670efdee9dedc87d
Christian Irving <Christian@Christians-MacBook-Pro.local>
2020-06-19 16:59:07 -0400

    bombpositionchanges

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className="row">
             <div className="logoContainer col-xs-6 valign"><img src={Logo} alt="Company Logo" className="logo" /></div>
-            <p className="col-xs-offset-4 col-xs-2  calign">💣</p>
+            <p className="bomb col-xs-offset-4 col-md-offset-5 col-xs-2 col-md-1  calign"><span role="img" aria-label="bomb">💣</span></p>
         </header >);
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,5 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-const Home = () => (<p>sdfsdfds sdf sd <Link to="/xavierspage">yo check this, bro</Link></p>);
+const Home = () => {
+    return (
+        <div className="container">
+            <div className="row center-xs">
+                <div className="col-xs-10 col-md-6 searchBar calign row">
+                    <div className="col-xs-6"><p>sdfsdfds sdf sd <Link to="/xavierspage">yo check this, bro</Link></p>  </div>
+                    <div className="col-xs-6">S</div>
+                </div>
+            </div>
+            <div className="row">
+
+            </div>
+        </div>
+        
+    )};
+
+
 export default Home;
