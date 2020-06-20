fe29620f296609416fd7fc65eec5c390f883c544
Christian Irving <Christian@Christians-MacBook-Pro.local>
2020-06-19 21:43:04 -0400

    betweenboxes and put menu icon needs tweaks

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Logo from './Asset 11.svg';
+import menubar from './icons8menu.svg'
 
 const Header = () => {
     return (
         <header className="row">
             <div className="logoContainer col-xs-6 valign"><img src={Logo} alt="Company Logo" className="logo" /></div>
-            <p className="bomb col-xs-offset-4 col-md-offset-5 col-xs-2 col-md-1  calign"><span role="img" aria-label="bomb">💣</span></p>
+            <div className="logoContainer col-xs-6 valign"><img src={menubar} alt="Header Menu" className="menu" /></div>
         </header >);
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,12 +7,82 @@ const Home = () => {
         <div className="container">
             <div className="row center-xs">
                 <div className="col-xs-10 col-md-6 searchBar calign row">
-                    <div className="col-xs-6"><p>sdfsdfds sdf sd <Link to="/xavierspage">yo check this, bro</Link></p>  </div>
-                    <div className="col-xs-6">S</div>
+                    <div className="col-xs-6"><p>Search Now...</p></div>
                 </div>
             </div>
-            <div className="row">
-
+            <div className="row between-xs">
+                <div className="col-xs-6
+                                col-sm-4
+                                col-md-3">
+                    <div class="box1">
+                        between
+                    </div>
+                </div>
+                <div className="col-xs-6
+                                col-sm-4
+                                col-md-3">
+                    <div class="box2">
+                        between
+                    </div>
+                </div>
+                <div className="col-xs-6
+                                col-sm-4
+                                col-md-3">
+                    <div class="box3">
+                        between
+                    </div>
+                </div>
+                <div className="col-xs-6
+                                col-sm-4
+                                col-md-3">
+                    <div class="box4">
+                        between
+                    </div>
+                </div>
+            </div>
+            <div className="row between-xs">
+            <div className="col-xs-6
+                                col-sm-4
+                                col-md-4">
+                    <div class="box5">
+                        between
+                    </div>
+                </div>
+                <div className="col-xs-6
+                                col-sm-4
+                                col-md-4">
+                    <div class="box6">
+                        between
+                    </div>
+                </div>
+                <div className="col-xs-6
+                                col-sm-4
+                                col-md-4">
+                    <div class="box7">
+                        between
+                    </div>
+                </div>
+                <div className="col-xs-6
+                                col-sm-4
+                                col-md-4">
+                    <div class="box8">
+                        between
+                    </div>
+                </div>
+                <div className="col-xs-6
+                                col-sm-4
+                                col-md-4">
+                    <div class="box9">
+                        between
+                    </div>
+                </div>
+                <div className="col-xs-6
+                                col-sm-4
+                                col-md-4">
+                    <div class="box10">
+                        between
+                    </div>
+                </div>
             </div>
         </div>
         
