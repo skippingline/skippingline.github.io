a932518509995f37d9284de2cf0e6fe8fb8e84e2
Christian Irving <Christian@Christians-MacBook-Pro.local>
2020-06-21 22:59:26 -0400

    Xaviers->category page issues with tiles + format

--- src/pages/Xavier.js
+++ src/pages/Xavier.js
@@ -1,6 +1,71 @@
 import React from 'react';
 
-const Xavier = () => (<>Welcome to Xavier's lair.🐲</>);
+const Xavier = () => {
+    return (
+        <div className="row between-xs categorypageboxes">
+            <div className="col-xs-12
+                        col-sm-6
+                        col-md-3">
+                <div>
+                <div className="row between-xs categorypageboxes1"></div>
+                </div>
+            </div>
+            <div className="col-xs-12
+                        col-sm-6
+                        col-md-3">
+                <div>
+                <div className="row between-xs categorypageboxes2"></div>
+                </div>
+            </div>
+            <div className="col-xs-12
+                        col-sm-6
+                        col-md-3">
+                <div>
+                <div className="row between-xs categorypageboxes3"></div>
+                </div>
+            </div>
+            <div className="col-xs-12
+                        col-sm-6
+                        col-md-3">
+                <div>
+                <div className="row between-xs categorypageboxes4"></div>
+                </div>
+            <div classname="col-xs-12
+                        col-sm-6
+                        col-md-3">
+                <div>
+                <div classname="row between-xs categorypageboxes5"></div>
+                </div>
+                <div classname="col-xs-12
+                        col-sm-6
+                        col-md-3">
+                <div>
+                <div classname="row between-xs categorypageboxes5"></div>
+                </div>
+
+             </div>
+                <div classname="col-xs-12
+                        col-sm-6
+                        col-md-3">
+                <div>
+                <div classname="row between-xs categorypageboxes5"></div>
+                </div>
+
+             </div>
+                <div classname="col-xs-12
+                        col-sm-6
+                        col-md-3">
+                <div>
+                <div classname="row between-xs categorypageboxes5"></div>
+                </div>
+
+                </div>
+             </div>
+        </div>
+    </div>
+
+    );
+}
 
 
 export default Xavier;
