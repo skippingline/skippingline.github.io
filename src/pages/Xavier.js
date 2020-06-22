87b5eaa76c3cdb67648f9d4d3cb400340bf34c54
Christian Irving <Christian@Christians-MacBook-Pro.local>
2020-06-22 08:37:27 -0400

    icons text and color to cat page

--- src/pages/Xavier.js
+++ src/pages/Xavier.js
@@ -7,28 +7,36 @@ const Xavier = () => {
                         col-sm-6
                         col-md-3">
                 <div>
-                <div className="row between-xs categorypageboxes1"></div>
+                <div className="row between-xs categorypageboxes1"><svg id="svgcat1" xmlns="http://www.w3.org/2000/svg" width="50px" height= "50px" viewBox="0 0 24 24" style={{fill:'#ffffff'}}><path d="M0 0h24v24H0z" fill="none"/><path d="M11 9H9V2H7v7H5V2H3v7c0 2.12 1.66 3.84 3.75 3.97V22h2.5v-9.03C11.34 12.84 13 11.12 13 9V2h-2v7zm5-3v8h2.5v8H21V2c-2.76 0-5 2.24-5 4z"/></svg>
+                <a>Restaurant</a>
+                </div>
                 </div>
             </div>
             <div className="col-xs-12
                         col-sm-6
                         col-md-3">
                 <div>
-                <div className="row between-xs categorypageboxes2"></div>
+                <div className="row between-xs categorypageboxes2"><svg id="svgcat2" xmlns="http://www.w3.org/2000/svg" width="50px" height= "50px" viewBox="0 0 24 24" style={{fill:'#ffffff'}}><g><rect fill="none" height="12" width="12"/><path d="M18,6h-2c0-2.21-1.79-4-4-4S8,3.79,8,6H6C4.9,6,4,6.9,4,8v12c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2V8C20,6.9,19.1,6,18,6z M10,10c0,0.55-0.45,1-1,1s-1-0.45-1-1V8h2V10z M12,4c1.1,0,2,0.9,2,2h-4C10,4.9,10.9,4,12,4z M16,10c0,0.55-0.45,1-1,1 s-1-0.45-1-1V8h2V10z"/></g></svg>
+                <a>Shopping</a>
+                </div>
                 </div>
             </div>
             <div className="col-xs-12
                         col-sm-6
                         col-md-3">
                 <div>
-                <div className="row between-xs categorypageboxes3"></div>
+                <div className="row between-xs categorypageboxes3"><svg id="svgcat3" xmlns="http://www.w3.org/2000/svg" width="50px" height= "50px" viewBox="0 0 24 24" style={{fill:'#ffffff'}}><path d="M0 0h24v24H0z" fill="none"/><path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"/></svg>
+                <a>Grocery</a>
+                </div>
                 </div>
             </div>
             <div className="col-xs-12
                         col-sm-6
                         col-md-3">
                 <div>
-                <div className="row between-xs categorypageboxes4"></div>
+                <div className="row between-xs categorypageboxes4"><svg id="svgcat4" xmlns="http://www.w3.org/2000/svg" width="50px" height= "50px" viewBox="0 0 24 24" style={{fill:'#ffffff'}}><path d="M0 0h24v24H0z" fill="none"/><path d="M18 4l2 4h-3l-2-4h-2l2 4h-3l-2-4H8l2 4H7L5 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4h-4z"/>
+                </svg><a>Entertainment</a>
+                </div>
                 </div>
             <div classname="col-xs-12
                         col-sm-6
@@ -40,7 +48,7 @@ const Xavier = () => {
                         col-sm-6
                         col-md-3">
                 <div>
-                <div classname="row between-xs categorypageboxes5"></div>
+                <div classname="row between-xs categorypageboxes6"></div>
                 </div>
 
              </div>
@@ -48,7 +56,7 @@ const Xavier = () => {
                         col-sm-6
                         col-md-3">
                 <div>
-                <div classname="row between-xs categorypageboxes5"></div>
+                <div classname="row between-xs categorypageboxes7"></div>
                 </div>
 
              </div>
@@ -56,7 +64,7 @@ const Xavier = () => {
                         col-sm-6
                         col-md-3">
                 <div>
-                <div classname="row between-xs categorypageboxes5"></div>
+                <div classname="row between-xs categorypageboxes8"></div>
                 </div>
 
                 </div>
