1a8de02eaaf65bd085ad86dad7f8d12bdf27f0e6
Christian Irving <Christian@Christians-MacBook-Pro.local>
2020-06-22 16:22:34 -0400

    restaurantpage

---
+++ src/pages/restaurantpage.js
@@ -0,0 +1,39 @@
+import react from "react";
+
+const restpage = () => {
+    return(
+        <div className="row between-xs restpageboxes">
+        <div className="col-xs-6
+                        col-sm-4
+                        col-md-3">
+            <div>
+                Image Here
+            </div>
+        </div>
+        <div className="col-xs-6
+                        col-sm-4
+                        col-md-3">
+            <div>
+                Image Here
+            </div>
+        </div>
+        <div className="col-xs-6
+                        col-sm-4
+                        col-md-3">
+            <div>
+                Image Here
+            </div>
+        </div>
+        <div className="col-xs-6
+                        col-sm-4
+                        col-md-3">
+            <div>
+                Image Here
+            </div>
+        </div>
+    </div>
+    );
+
+}
+
+export default restpage;
