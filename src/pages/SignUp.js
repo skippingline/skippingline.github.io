65009d6553202f9e8c24988356d2fd70b98ef4cc
Christian Irving <Christian@Christians-MacBook-Pro.local>
2020-06-21 22:27:06 -0400

    start to the signup-in page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,7 +2,22 @@ import React from "react";
 
 const SignUp = () => {
 
-    return (<div>hello</div>);
+    return (
+        <div className="row betweeen xs signupbox">
+            <div className="col-xs-6
+                            col-sm-4
+                            col-md-3">
+                <div>
+                <div className="namesignuptext">Name</div>
+                <input type="text" className="col-xs-10 col-md-6 signupname"/>
+                <div className="emailsignuptext">Email</div>
+                <input type="text" className="col-xs-10 col-md-6 signupemail"/>
+                <div className="passwordsignuptext">Password</div>
+                <input type="text" className="col-xs-10 col-md-6 signuppassword"/>
+                </div>
+             </div>
+        </div>
+        );
 }
 
 export default SignUp;
