e16067d6404d2254a317d62444db53e7138e00de
Christian Irving <Christian@Christians-MacBook-Pro.local>
2020-06-21 20:35:21 -0400

    adding cat icons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -45,42 +45,42 @@ const Home = () => {
                                 col-sm-4
                                 col-md-4">
                     <div>
-                        between
+                    <svg id="svg1" xmlns="http://www.w3.org/2000/svg" width="50px" height= "50px" viewBox="0 0 24 24" style={{fill:'#cccccc'}}><path d="M0 0h24v24H0z" fill="none"/><path d="M11 9H9V2H7v7H5V2H3v7c0 2.12 1.66 3.84 3.75 3.97V22h2.5v-9.03C11.34 12.84 13 11.12 13 9V2h-2v7zm5-3v8h2.5v8H21V2c-2.76 0-5 2.24-5 4z"/></svg>
                     </div>
                 </div>
                 <div className="col-xs-6
                                 col-sm-4
                                 col-md-4">
                     <div>
-                        between
+                    <svg id="svg2" xmlns="http://www.w3.org/2000/svg" width="50px" height= "50px" viewBox="0 0 24 24" style={{fill:'#cccccc'}}><g><rect fill="none" height="12" width="12"/><path d="M18,6h-2c0-2.21-1.79-4-4-4S8,3.79,8,6H6C4.9,6,4,6.9,4,8v12c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2V8C20,6.9,19.1,6,18,6z M10,10c0,0.55-0.45,1-1,1s-1-0.45-1-1V8h2V10z M12,4c1.1,0,2,0.9,2,2h-4C10,4.9,10.9,4,12,4z M16,10c0,0.55-0.45,1-1,1 s-1-0.45-1-1V8h2V10z"/></g></svg>
                     </div>
                 </div>
                 <div className="col-xs-6
                                 col-sm-4
                                 col-md-4">
                     <div>
-                        between
+                    <svg id="svg3" xmlns="http://www.w3.org/2000/svg" width="50px" height= "50px" viewBox="0 0 24 24" style={{fill:'#cccccc'}}><path d="M0 0h24v24H0z" fill="none"/><path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z"/></svg>
                     </div>
                 </div>
                 <div className="col-xs-6
                                 col-sm-4
                                 col-md-4">
                     <div>
-                        between
+                    <svg id="svg4" xmlns="http://www.w3.org/2000/svg" width="50px" height= "50px" viewBox="0 0 24 24" style={{fill:'#cccccc'}}><path d="M0 0h24v24H0z" fill="none"/><path d="M18 4l2 4h-3l-2-4h-2l2 4h-3l-2-4H8l2 4H7L5 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4h-4z"/></svg>
                     </div>
                 </div>
                 <div className="col-xs-6
                                 col-sm-4
                                 col-md-4">
                     <div>
-                        between
+                    <svg id="svg5" xmlns="http://www.w3.org/2000/svg" width="45px" height= "45px" viewBox="0 0 24 24" style={{fill:'#cccccc'}} ><path d="M0 0h24v24H0z" fill="none"/><path d="M20.57 14.86L22 13.43 20.57 12 17 15.57 8.43 7 12 3.43 10.57 2 9.14 3.43 7.71 2 5.57 4.14 4.14 2.71 2.71 4.14l1.43 1.43L2 7.71l1.43 1.43L2 10.57 3.43 12 7 8.43 15.57 17 12 20.57 13.43 22l1.43-1.43L16.29 22l2.14-2.14 1.43 1.43 1.43-1.43-1.43-1.43L22 16.29z"/></svg>
                     </div>
                 </div>
                 <div className="col-xs-6
                                 col-sm-4
                                 col-md-4">
                     <div>
-                        between
+                    <svg id="svg6" xmlns="http://www.w3.org/2000/svg" width="50px" height= "50px" viewBox="0 0 24 24" style={{fill:'#cccccc'}}><path d="M0 0h24v24H0z" fill="none"/><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg>
                     </div>
                 </div>
             </div>
